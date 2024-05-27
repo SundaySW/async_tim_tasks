a7a50792c1f68b658ab174ffebd97df69129119e
SundaySW <9162101027@mail.ru>
2024-05-27 18:06:02 +0300

    refactoring

--- async_tim_tasks.hpp
+++ async_tim_tasks.hpp
@@ -7,29 +7,29 @@
        place macro ASYNC_TASKS_ON_TIM() in interrupt handler for desired timer
        place macro ASYNC_TASKS_POLL() im main loop
        place macro TASK_POOL_SET_UP (
-               1st @arg -> converting Hz to delay lambda [](float arg){... return uint32_t delay;}
-               2nd @arg -> starting lambda []{ StartTim(tim); }
+               1st @param lambda -> converting Hz to delay [](float arg){... return uint32_t delay;}
+               2nd @param lambda -> starting []{ StartTim(tim); }
            )
       to place a task:
           type macro PLACE_ASYNC_TASK(
-                1st @arg -> lambda [](void* self_ptr_as_context){static_cast<SelfT*>(context)->...}
-                2nd @arg -> time in Hz for this task to repeat
+                1st @param lambda -> [](void* self_ptr_as_context){static_cast<SelfT*>(context)->...}
+                2nd @param float -> time in Hz for this task to repeat
             )
             @retval task N in pool(std::size_t)
 
           to put max frequently task use PLACE_ASYNC_QUICKEST(
-                @arg -> lambda [](void* self_ptr_as_context){static_cast<SelfT*>(context)->...}
+                @param lambda -> [](void* self_ptr_as_context){static_cast<SelfT*>(context)->...}
             )
             @retval task N in pool(std::size_t)
 
       to stop a task:
       use macro STOP_ASYNC_TASK(
-            @arg -> task N which was a retval for PLACE_ASYNC_TASK_...
+            @param taskN -> which was a retval for PLACE_ASYNC_TASK_...
           )
 
       to resume a task:
       use macro RESUME_ASYNC_TASK(
-            @arg -> task N which was a retval for PLACE_ASYNC_TASK_...
+            @param taskN -> which was a retval for PLACE_ASYNC_TASK_...
           )
 
       @param pool_size - default to 10 in impl/task_pool.hpp
@@ -55,7 +55,7 @@ namespace async_tim_task{
                     async_tim_task_impl::TaskPool::GetPool().PlaceToPool({this, lambda}, Hz, true)
 
 #define PLACE_ASYNC_SUSPENDED_QUICKEST(lambda)  \
-                    async_tim_task_impl::TaskPool::GetPool().PlaceToPool(task::CB(this, lambda), true)
+                    async_tim_task_impl::TaskPool::GetPool().PlaceToPool({this, lambda}, true)
 
 #define REMOVE_ASYNC_TASK(n)  async_tim_task_impl::TaskPool::GetPool().RemoveFromPool(n)
 #define STOP_ASYNC_TASK(n)  async_tim_task_impl::TaskPool::GetPool().StopTask(n)
