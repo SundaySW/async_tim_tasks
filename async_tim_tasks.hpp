881156257f07f041e27d8b768b9902cec13801fd
SundaySW <9162101027@mail.ru>
2024-07-26 13:28:41 +0300

    usage macro remastering

--- async_tim_tasks.hpp
+++ async_tim_tasks.hpp
@@ -4,31 +4,31 @@
 
 /**
     * @brief to use:
-       ASYNC_TASKS_ON_TIM place in interrupt handler for desired timer
-       ASYNC_TASKS_POLL place im main loop
-       place macro ASYNC_TASKS_POOL_SET_UP(
+       $AsyncTasksOnTim place in interrupt handler for desired timer
+       $AsyncTasksPoll place im main loop
+       place macro $AsyncTasksPoolSetUp(
                1st @param lambda -> converting Hz to delay [](float arg)->uint32_t {... return delay;}
                2nd @param lambda -> starting []->void { StartTim(tim); }
            )
       to place a task:
-            RUN_ASYNC_hz(
+            $RunAsync(
                 1st @param expression -> expression in braces. use @var self for context {self->...}
                 2nd @param float -> time in Hz for this task to repeat
             )
             @retval task N in pool(std::size_t)
 
-            to put max frequently task RUN_ASYNC(
+            to put max frequently task $RunAsync(
                 1st @param expression -> expression in braces. @var self for context {self->...}
             )
             @retval task N in pool(std::size_t)
 
       to stop a task:
-      use macro STOP_ASYNC_TASK(
+      use macro $StopAsyncTask(
             @param taskN -> which was a retval for PLACE_ASYNC_TASK_...
           )
 
       to resume a task:
-      use macro RESUME_ASYNC_TASK(
+      use macro $ResumeAsyncTask(
             @param taskN -> which was a retval for PLACE_ASYNC_TASK_...
           )
 
@@ -37,46 +37,54 @@
 
 namespace async_tim_task{
 
-#define ASYNC_TASKS_POOL_SET_UP(converter, starter) async_tim_task_impl::TaskPool::GetPool().SetUp(converter, starter)
-#define ASYNC_TASKS_ON_TIM async_tim_task_impl::TaskPool::GetPool().OnTimTick();
-#define ASYNC_TASKS_POLL async_tim_task_impl::TaskPool::GetPool().Poll();
+#define $AsyncTasksPoolSetUp(converter, starter) async_tim_task_impl::TaskPool::GetPool().SetUp(converter, starter)
+#define $AsyncTasksOnTim async_tim_task_impl::TaskPool::GetPool().OnTimTick();
+#define $AsyncTasksPoll async_tim_task_impl::TaskPool::GetPool().Poll();
 
-#define RUN_ASYNC(expr) \
+#define $RunAsync(...) $RunAsyncMacroChooser(__VA_ARGS__)(__VA_ARGS__)
+#define $RunAsyncStatic(...) $RunAsyncStaticMacroChooser(__VA_ARGS__)(__VA_ARGS__)
+#define $RegAsyncSuspended(...) $RegAsyncMacroChooser(__VA_ARGS__)(__VA_ARGS__)
+
+#define $RemoveAsyncTask(n) async_tim_task_impl::TaskPool::GetPool().RemoveFromPool(n)
+#define $StopAsyncTask(n) async_tim_task_impl::TaskPool::GetPool().StopTask(n)
+#define $ResumeAsyncTask(n) async_tim_task_impl::TaskPool::GetPool().ResumeTask(n)
+
+
+#define $get_override(arg1, arg2, arg3, ...) arg3
+
+#define $RunAsyncMacroChooser(...) $get_override(__VA_ARGS__, $RunAsync_hz, $RunAsync_quickest)
+#define $RunAsync_quickest(expr) \
     async_tim_task_impl::TaskPool::GetPool().PlaceToPool(async_tim_task_impl::CallBackT(this, [](void* context){    \
             auto self = static_cast<decltype(this)>(context);                                                       \
             expr;                                                                                                   \
         }                                                                                                           \
     ))
-
-#define RUN_ASYNC_hz(expr, Hz)  \
+#define $RunAsync_hz(expr, Hz)  \
     async_tim_task_impl::TaskPool::GetPool().PlaceToPool(async_tim_task_impl::CallBackT(this, [](void* context){    \
             auto self = static_cast<decltype(this)>(context);                                                       \
             expr;                                                                                                   \
         }                                                                                                           \
     ), Hz)
 
-#define RUN_ASYNC_no_context(expr, Hz)  \
+#define $RunAsyncStaticMacroChooser(...) $get_override(__VA_ARGS__, $RunAsync_static_hz, $RunAsyncStatic_quickest)
+#define $RunAsync_static_hz(expr, Hz)  \
         async_tim_task_impl::TaskPool::GetPool().PlaceToPool(async_tim_task_impl::CallBackT([](void* context)expr), Hz)
-
-#define RUN_ASYNC_no_context_quickest(expr)  \
+#define $RunAsyncStatic_quickest(expr)  \
         async_tim_task_impl::TaskPool::GetPool().PlaceToPool(async_tim_task_impl::CallBackT([](void* context)expr))
 
-#define REGISTER_ASYNC_TASK_suspended(expr, Hz)  \
+#define $RegAsyncMacroChooser(...) $get_override(__VA_ARGS__, $RegAsyncTask_suspended, $RegAsyncInitial_suspended_quickest)
+#define $RegAsyncTask_suspended(expr, Hz)  \
     async_tim_task_impl::TaskPool::GetPool().PlaceToPool(async_tim_task_impl::CallBackT(this, [](void* context){    \
             auto self = static_cast<decltype(this)>(context);                                                       \
             expr;                                                                                                   \
         }                                                                                                           \
     ), Hz, true)
 
-#define RUN_ASYNC_initial_suspend_quickest(expr)  \
+#define $RegAsyncInitial_suspended_quickest(expr)  \
     async_tim_task_impl::TaskPool::GetPool().PlaceToPool(async_tim_task_impl::CallBackT(this, [](void* context){    \
             auto self = static_cast<decltype(this)>(context);                                                       \
             expr;                                                                                                   \
         }                                                                                                           \
     ), true)
-
-#define REMOVE_ASYNC_TASK(n) async_tim_task_impl::TaskPool::GetPool().RemoveFromPool(n)
-#define STOP_ASYNC_TASK(n) async_tim_task_impl::TaskPool::GetPool().StopTask(n)
-#define RESUME_ASYNC_TASK(n) async_tim_task_impl::TaskPool::GetPool().ResumeTask(n)
 
 }// namespace async_tim_task
